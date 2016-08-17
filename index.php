827b07b251f387bd053132ae36494e85d842d1e2
toton333 <toton333@yahoo.com>
2016-08-17 15:30:47 +0530

    deployed via deployhq

--- index.php
+++ index.php
@@ -11,6 +11,7 @@
     Item 2:<input type="checkbox" class="children" value="item2">
     Item 3:<input type="checkbox" class="children" value="item3">
   </p>
+  <p>Note: deployed via deployhq</p>
 
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
     <script type="text/javascript" src="selector.js"></script>
